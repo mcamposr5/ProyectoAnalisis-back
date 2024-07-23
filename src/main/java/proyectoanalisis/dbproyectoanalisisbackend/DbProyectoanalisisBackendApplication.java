001877c38b31d8b837545fc672287f096d3baa9d
Miguel Campos <140900175+macamposr@users.noreply.github.com>
2024-07-23 17:51:07 -0600

    Initial commit

---
+++ src/main/java/proyectoanalisis/dbproyectoanalisisbackend/DbProyectoanalisisBackendApplication.java
@@ -0,0 +1,13 @@
+package proyectoanalisis.dbproyectoanalisisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbProyectoanalisisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbProyectoanalisisBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/proyectoanalisis/dbproyectoanalisisbackend/DbProyectoanalisisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectoanalisis.dbproyectoanalisisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProyectoanalisisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
